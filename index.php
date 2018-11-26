5123a47fe7cb88b7802af47de04562630148e1d4
Lehi Qr <lehi_10@hotmail.com>
2018-11-26 07:30:35 -0500

    Update

--- index.php
+++ index.php
@@ -22,12 +22,11 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>                        
       </button>
-      <a class="navbar-brand" href="#">Logo</a>
+      <a class="navbar-brand" href="#">Aguada Blanca</a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
+        <li class="active"><a href="#">Inicio</a></li>
       </ul>
     </div>
   </div>
@@ -35,31 +34,170 @@
 
   
 <div class="row">
+  	<center><h3>Probe Data</h3></center>
+  	<br>
   <div class="col-sm-6">
-  <h4>Ph</h4>
-  	<span id="counter"><?php echo $data['counter']; ?></span>
+  
+	  <div class="row-fluid">
+	    <form class="form-horizontal" action="/action_page.php">
+    <div class="form-group">
+      <label class="control-label col-sm-4"> Ph:</label>
+      <div class="col-sm-8">
+		<input type="text" class="form-control" id="ph" placeholder="Loading ..." >
+      </div>
+    </div>
+    
+    <div class="form-group">
+      <label class="control-label col-sm-4"> Conductivity:</label>
+      <div class="col-sm-8">
+		<input type="text" class="form-control" placeholder="0" >
+      </div>
+    </div>
+    
+    <div class="form-group">
+      <label class="control-label col-sm-4"> Turbidity:</label>
+      <div class="col-sm-8">
+		<input type="text" class="form-control" placeholder="0" >
+      </div>
+    </div>
+    
+    <div class="form-group">
+      <label class="control-label col-sm-4"> RDO:</label>
+      <div class="col-sm-8">
+		<input type="text" class="form-control" placeholder="0" >
+      </div>
+    </div>
+    
+    <div class="form-group">
+      <label class="control-label col-sm-4"> TSS:</label>
+      <div class="col-sm-8">
+		<input type="text" class="form-control" placeholder="0" >
+      </div>
+    </div>
+    
+    <div class="form-group">
+      <label class="control-label col-sm-4"> Nitrate:</label>
+      <div class="col-sm-8">
+		<input type="text" class="form-control" placeholder="0" >
+      </div>
+    </div>
+    
+    
+    <div class="form-group">
+      <label class="control-label col-sm-4"> Ammonium:</label>
+      <div class="col-sm-8">
+		<input type="text" class="form-control" placeholder="0" >
+      </div>
+    </div>
+    
+  </form>
+  
+	</div>
+		  	
   </div>
+  
   <div class="col-sm-6">
-  	<img width="80%" src="vehicle.jpeg">
+  	<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d9199.644229532669!2d-71.3381293308248!3d-16.239018912103933!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1ses-419!2spe!4v1543189027399" width="530" height="360" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+
   </div>
+  
 </div>
 
+<hr>
+
+<div class="row">
+  	<center><h3>Vehicle Data</h3></center>
+  	<br>  	
+  <div class="col-sm-6">
+
+
+  
+	  <div class="row-fluid">
+	    <form class="form-horizontal" action="/action_page.php">
+    <div class="form-group">
+      <label class="control-label col-sm-4"> Speed:</label>
+      <div class="col-sm-8">
+		<input type="text" class="form-control" id="ph" placeholder="Loading ..." >
+      </div>
+    </div>
+    
+    <div class="form-group">
+      <label class="control-label col-sm-4"> Roll:</label>
+      <div class="col-sm-8">
+		<input type="text" class="form-control" placeholder="0" >
+      </div>
+    </div>
+    
+    <div class="form-group">
+      <label class="control-label col-sm-4"> Pitch:</label>
+      <div class="col-sm-8">
+		<input type="text" class="form-control" placeholder="0" >
+      </div>
+    </div>
+    
+    <div class="form-group">
+      <label class="control-label col-sm-4"> Yaw:</label>
+      <div class="col-sm-8">
+		<input type="text" class="form-control" placeholder="0" >
+      </div>
+    </div>
+    
+    <div class="form-group">
+      <label class="control-label col-sm-4"> Latitude:</label>
+      <div class="col-sm-8">
+		<input type="text" class="form-control" placeholder="0" >
+      </div>
+    </div>
+    
+    <div class="form-group">
+      <label class="control-label col-sm-4"> Longitude:</label>
+      <div class="col-sm-8">
+		<input type="text" class="form-control" placeholder="0" >
+      </div>
+    </div>
+    
+    
+    <div class="form-group">
+      <label class="control-label col-sm-4"> Temperature:</label>
+      <div class="col-sm-8">
+		<input type="text" class="form-control" placeholder="0" >
+      </div>
+    </div>
+    
+	<div class="form-group">
+      <label class="control-label col-sm-4"> Baterry:</label>
+      <div class="col-sm-8">
+		<input type="text" class="form-control" placeholder="0" >
+      </div>
+    </div>
+    
+  </form>
+  
+	</div>
+		  	
+  </div>
+  
+  <div class="col-sm-6">
+    	<img width="80%" src="vehicle.jpeg">
+
+  </div>
+  
+</div>
 
 
 
 <script type="text/javascript">
 $(document).ready(function() {	
     function update(){
-        var current = $('#counter').text();
-        var sum = Number(current) + 3;
-        var dataString = 'sum='+sum;
+        var dataString = "";
  
         $.ajax({
             type: "POST",
             url: "save.php",
             data: dataString,
             success: function(response) {
-                $('#counter').text(response);
+                $('#ph').val(response);
             }
         });
     }
